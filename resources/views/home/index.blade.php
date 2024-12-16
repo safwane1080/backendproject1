48c330dbd119781b03ffb6e5dd2e4868171bb119
safwane1080 <safwane.el.masaoudi@student.ehb.be>
2024-12-16 15:50:07 +0100

    Admin pagina
    Admin pagina is klaar, routes moeten nog gemaakt worden.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -140,6 +140,11 @@
         <li class="nav-item">
             <a class="nav-link" href="#contact">Contact</a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/login">Login</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/register">Register
     </ul>
 </nav>
 
